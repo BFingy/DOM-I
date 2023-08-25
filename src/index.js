6d849ac81e4da451c8ef01e0f1803e64bfa532ee
Brandon Finger <bfinger.work@yahoo.com>
2023-08-24 20:26:31 -0500

    final

--- src/index.js
+++ src/index.js
@@ -39,4 +39,68 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
   },
 };
 
-console.log('project wired!')
+
+//IMAGES
+const logoImg = document.querySelector('#logo-img');
+logoImg.src = siteContent.images['logo-img'];
+
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src = siteContent.images['cta-img'];
+
+const lastImg = document.querySelector('#middle-img');
+lastImg.src = siteContent.images['accent-img'];
+
+
+//FOOTER LINK
+const footLink = document.querySelector('footer a');
+footLink.textContent = siteContent.footer.copyright;
+footLink.classList.add('bold');
+
+//CONTACT
+const contactBar = document.querySelector('section.contact');
+contactBar.children[0].textContent = siteContent.contact['contact-h4'];
+contactBar.children[1].textContent = siteContent.contact.address;
+contactBar.children[2].textContent = siteContent.contact.phone;
+contactBar.children[3].textContent = siteContent.contact.email;
+
+//TOP CONTENT
+const topContent = document.querySelector('.top-content');
+topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4'];
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content'];
+topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4'];
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content'];
+
+//BOTTOM CONTENT
+const bottomContent = document.querySelector('.bottom-content');
+const h4Bottom = bottomContent.querySelectorAll('h4')
+h4Bottom[0].textContent = siteContent['main-content']['services-h4'];
+h4Bottom[1].textContent = siteContent['main-content']['product-h4'];
+h4Bottom[2].textContent = siteContent['main-content']['vision-h4'];
+const pBottom = bottomContent.querySelectorAll('p');
+pBottom[0].textContent = siteContent['main-content']['services-content'];
+pBottom[1].textContent = siteContent['main-content']['product-content'];
+pBottom[2].textContent = siteContent['main-content']['vision-content'];
+
+//CTA BUTTON
+const ctaText = document.querySelector('h1')
+ctaText.textContent = siteContent.cta.h1;
+const ctaButton = document.querySelector('button');
+ctaButton.textContent = siteContent.cta.button;
+
+
+
+//NAV LINK
+const navLink = document.querySelectorAll('nav a');
+navLink[0].textContent = siteContent.nav['nav-item-1']
+navLink[1].textContent = siteContent.nav['nav-item-2']
+navLink[2].textContent = siteContent.nav['nav-item-3']
+navLink[3].textContent = siteContent.nav['nav-item-4']
+navLink[4].textContent = siteContent.nav['nav-item-5']
+navLink[5].textContent = siteContent.nav['nav-item-6']
+
+navLink[0].classList.add('italic');
+navLink[1].classList.add('italic');
+navLink[2].classList.add('italic');
+navLink[3].classList.add('italic');
+navLink[4].classList.add('italic');
+navLink[5].classList.add('italic');
